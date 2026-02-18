beedbccce73b903326e718236d65d2b1d4e2e4cf
JcDub625 <joshua@wideman.com>
2026-02-17 22:08:38 -0600

    Bugfix 2

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
-import * as THREE from "https://unpkg.com/three@0.150.1/build/three.module.js";
-import { MindARThree } from "https://unpkg.com/mind-ar@1.2.5/dist/mindar-image-three.prod.js";
-
 const start = async () => {
-  const mindarThree = new MindARThree({
+  const mindarThree = new window.MINDAR.IMAGE.MindARThree({
     container: document.body,
     imageTargetSrc: "./targets.mind",
   });
@@ -11,11 +8,10 @@ const start = async () => {
 
   const anchor = mindarThree.addAnchor(0);
 
-  // Light
   const light = new THREE.HemisphereLight(0xffffff, 0xbbbbff, 1);
   scene.add(light);
 
-  // Create video element
+  // Create video
   const video = document.createElement("video");
   video.src = "./DemoVideo.mp4";
   video.loop = true;
@@ -23,10 +19,8 @@ const start = async () => {
   video.setAttribute("playsinline", "");
   video.crossOrigin = "anonymous";
 
-  // Create video texture
   const videoTexture = new THREE.VideoTexture(video);
 
-  // Create plane
   const geometry = new THREE.PlaneGeometry(1, 1);
   const material = new THREE.MeshBasicMaterial({
     map: videoTexture,
@@ -36,14 +30,8 @@ const start = async () => {
   const plane = new THREE.Mesh(geometry, material);
   anchor.group.add(plane);
 
-  // Target events
-  anchor.onTargetFound = () => {
-    video.play();
-  };
-
-  anchor.onTargetLost = () => {
-    video.pause();
-  };
+  anchor.onTargetFound = () => video.play();
+  anchor.onTargetLost = () => video.pause();
 
   await mindarThree.start();
 
